88605c1e902239ceafd577bdeeae0ce71abe90db
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2020-12-01 18:20:47 -0500

    Add brute_recur and dynamic_programming

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import tsp
 import pprint
+import sys
+from os.path import dirname, join
+from time import perf_counter_ns, time_ns
+
+# # Route print statements to file
+# current_dir = dirname(__file__)
+# file_name = "verification_" + str(time_ns()) + ".txt"
+# file_path = join(current_dir, file_name)
+# sys.stdout = open(file_path, "w")
 
 
 def main():
 
-    n = 10
-    max_cost = r = 9
+    n = 9
+    max_cost = r = 99
     m = tsp.generate_random_cost_matrix(n, max_cost)
     # m = tsp.generate_euclidean_cost_matrix(n, max_cost)
-    # m, min_distance = tsp.generate_circular_cost_matrix(n, r)
-    tsp.write_matrix_to_file(m, "matrix.txt")
+    # m, min_cost = tsp.generate_circular_cost_matrix(n, r)
+    # tsp.write_matrix_to_file(m, "matrix.txt")
 
-    m = tsp.read_matrix_from_file("matrix.txt")
-    tsp.matrix_print(m)
+    # m = tsp.read_matrix_from_file("matrix.txt")
     print(tsp.greedy(m))
-    print(tsp.brute(m))
+    print(tsp.brute_iterative(m))
+    print(tsp.brute_recur(m))
+    print(tsp.dynamic_programming(m))
 
-    g = tsp.matrix_to_graph(m)
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(g)
-
-    print("Verified:", tsp.verify_optimal_solutions())
+    # print("Verified:", tsp.verify_exact_algorithms())
 
 
 if __name__ == "__main__":
--- tsp.py
+++ tsp.py
@@ -45,17 +45,18 @@ def generate_circular_cost_matrix(num_v, r):
     vertices = [(r * math.cos(i * theta), r * math.sin(i * theta))
                 for i in range(num_v)]
 
-    min_distance = 0
+    min_cost = 0
     for i in range(len(vertices)):
         u = vertices[i]
         v = vertices[(i + 1) % num_v]
-        min_distance += get_distance(u, v)
+        min_cost += get_distance(u, v)
 
     # Put vertices in random order
     random.shuffle(vertices)
 
     # Return matrix and minimum distance
-    return coordinates_to_cost_matrix(vertices), min_distance
+    # return coordinates_to_cost_matrix(vertices), min_distance
+    return coordinates_to_cost_matrix(vertices), min_cost
 
 
 def get_distance(u, v):
@@ -64,42 +65,7 @@ def get_distance(u, v):
     x2 = v[0]
     y1 = u[1]
     y2 = v[1]
-    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-
-
-def greedy_first_attempt(m):
-    """Finds a path by always choosing the shortest edge"""
-    # Convert data structure from matrix to dictionary
-    g = matrix_to_graph(m)
-
-    # Start at vertex 0
-    v = 0
-    path = [0]
-    cost = 0
-
-    # Visit every vertex once
-    n = len(g)
-    for _ in range(n-1):
-        # Make a list of unvisited vertices
-        unvisited = [x for x in g[v] if x[0] not in path]
-
-        # Travel to the vertex with the least edge cost
-        min_edge_cost = sys.maxsize
-        for i in range(len(unvisited)):
-            edge_cost = unvisited[i][1]
-            if edge_cost < min_edge_cost:
-                min_edge_cost = edge_cost
-                v = unvisited[i][0]
-
-        # Add the vertex to the path and track cost
-        path.append(v)
-        cost += min_edge_cost
-
-    path.append(0)
-    final_edge_cost = [x[1] for x in g[v] if x[0] == 0]
-    cost += min(final_edge_cost)
-
-    return path, cost
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
 def greedy(m):
@@ -115,7 +81,7 @@ def greedy(m):
     cost = 0
 
     # Visit every other vertex
-    for _ in range(n-1):
+    for _ in range(n - 1):
         # Build list of unvisited vertices
         row = enumerate(m[u])
         unvisited = [x for x in row if x[0] in vertices]
@@ -140,29 +106,23 @@ def greedy(m):
     return path, cost
 
 
-def get_list_permutations(n):
-    """Returns all permutations of an n-length list"""
-    perms = itertools.permutations(range(n))
-    return perms
-
-
-def brute(m):
+def brute_iterative(m):
     """Finds optimal solution to the TSP by checking every possibility"""
     # Get permutations for an n-1 length list,
     # and sandwich each permutation with zeros
     # (we can always start and end at vertex 0)
     n = len(m)
-    perms = get_list_permutations(n-1)
-    perms = [[0] + [int(x)+1 for x in perm] + [0] for perm in perms]
+    perms = itertools.permutations(range(n - 1))
+    perms = [[0] + [int(x) + 1 for x in perm] + [0] for perm in perms]
 
     # Check the cost of each permutation
     min_cost = sys.maxsize
     for p in perms:
         cost = 0
         # Calculate cost from 0 -> ... -> 0
-        for i in range(1, n+1):
+        for i in range(1, n + 1):
             curr_v = p[i]
-            prev_v = p[i-1]
+            prev_v = p[i - 1]
             cost += m[prev_v][curr_v]
         # Update min cost and min perm if shortest seen
         if cost < min_cost:
@@ -170,6 +130,128 @@ def brute(m):
             min_cost = cost
 
     return min_p, min_cost
+
+
+def brute_recur(m):
+    """Finds optimal solution to the TSP by checking every possibility"""
+
+    # Start and end with the same node to complete the circuit
+    start_node = end_node = 0
+
+    # Build list of unvisited nodes
+    tour_nodes = [x for x in range(len(m)) if x != start_node]
+
+    # Recursively find the minimum cost path between tour nodes
+    path, cost = brute_recur_helper(m, start_node, end_node, tour_nodes)
+
+    # Add starting node to beginning of the best path
+    path.insert(0, start_node)
+
+    return path, cost
+
+
+def brute_recur_helper(m, start_node, end_node, tour_nodes):
+    """Recursive helper function for brute_recur"""
+
+    # Base case: only one node left to visit
+    if len(tour_nodes) == 1:
+        last_tour_node = tour_nodes[0]
+
+        # Make a path from last tour node -> end node
+        path = tour_nodes + [end_node]
+
+        # Calculate the cost from k -> last tour node -> end node
+        cost = m[start_node][last_tour_node] + m[last_tour_node][end_node]
+
+        return path, cost
+    else:
+        min_cost = sys.maxsize
+        min_path = []
+
+        # Determine all possible permutations
+        for k in tour_nodes:
+
+            # Use each k in tour_nodes as a starting node
+            # for the recursive calls
+            unvisited = [x for x in tour_nodes if x != k]
+            tmp_path, tmp_cost = brute_recur_helper(m, k, end_node, unvisited)
+
+            # Take the branch with the least cost
+            best_cost_from_k = m[start_node][k] + tmp_cost
+            if best_cost_from_k < min_cost:
+                min_cost = best_cost_from_k
+                min_path = [k] + tmp_path
+
+        return min_path, min_cost
+
+
+def dynamic_programming(m):
+    """brute_recur with caching"""
+    # Start and end with the same node to complete the circuit
+    start_node = end_node = 0
+
+    # Build list of unvisited nodes
+    tour_nodes = [x for x in range(len(m)) if x != start_node]
+
+    # Init cache table for all possibilities
+    cache = []
+    n = len(m)
+    power_set_length = 2 ** n
+    for i in range(power_set_length):
+        cache.append([-1] * power_set_length)
+
+    # Recursively find the minimum cost path between tour nodes
+    path, cost = dynamic_programming_helper(m, start_node, end_node, tour_nodes, cache)
+
+    # Add starting node to beginning of the best path
+    path.insert(0, start_node)
+
+    return path, cost
+
+
+def dynamic_programming_helper(m, start_node, end_node, tour_nodes, cache):
+    """Recursive helper function for dynamic_programming"""
+
+    # Solution already in cache table
+    if cache[start_node][list_to_index(tour_nodes)] != -1:
+        return cache[start_node][list_to_index(tour_nodes)]
+    else:
+        # Base case: only one node left to visit
+        if len(tour_nodes) == 1:
+            last_tour_node = tour_nodes[0]
+
+            # Make a path from last tour node -> end node
+            path = tour_nodes + [end_node]
+
+            # Calculate the cost from k -> last tour node -> end node
+            cost = m[start_node][last_tour_node] + m[last_tour_node][end_node]
+
+            # Save solution in cache
+            cache[start_node][list_to_index(tour_nodes)] = path, cost
+
+            return path, cost
+        else:
+            min_cost = sys.maxsize
+            min_path = []
+
+            # Determine all possible permutations
+            for k in tour_nodes:
+
+                # Use each k in tour_nodes as a starting node
+                # for the recursive calls
+                unvisited = [x for x in tour_nodes if x != k]
+                tmp_path, tmp_cost = dynamic_programming_helper(m, k, end_node, unvisited, cache)
+
+                # Save solution in cache
+                cache[k][list_to_index(unvisited)] = tmp_path, tmp_cost
+
+                # Take the branch with the least cost
+                best_cost_from_k = m[start_node][k] + tmp_cost
+                if best_cost_from_k < min_cost:
+                    min_cost = best_cost_from_k
+                    min_path = [k] + tmp_path
+
+            return min_path, min_cost
 
 
 def matrix_to_graph(m):
@@ -220,14 +302,43 @@ def matrix_print(m):
             print()
 
 
-def verify_optimal_solutions():
+def verify_exact_algorithms():
     # Generate random circular cost matrices
     for i in range(4, 10):
-        matrix, min_distance = generate_circular_cost_matrix(i, 1000)
-        brute_path, brute_cost = brute(matrix)
+        matrix, min_cost = generate_circular_cost_matrix(i, 1000)
+        brute_iter_path, brute_iter_cost = brute_iterative(matrix)
+        brute_recur_path, brute_recur_cost = brute_recur(matrix)
+        dynamic_path, dynamic_cost = dynamic_programming(matrix)
+        print(f"Testing {i}x{i} matrix...", end="\n\n")
+        matrix_print(matrix)
+        print()
+        print(f"Brute Iterative: \t{brute_iter_path} \tCost: {brute_iter_cost}")
+        print(f"Brute Recursive: \t{brute_recur_path} \tCost: {brute_recur_cost}")
+        print(f"Dynamic Programming: \t{dynamic_path} \tCost: {dynamic_cost}")
+        print()
 
         # Compare solution with minimum distance around the circle
-        if round(brute_cost, 6) != round(min_distance, 6):
-            print(brute_cost, min_distance)
+        precision = 8
+        if round(brute_recur_cost, precision) != round(min_cost, precision) \
+                or round(brute_iter_cost, precision) != round(min_cost, precision) \
+                or round(dynamic_cost, precision) != round(min_cost, precision):
             return False
     return True
+
+
+def list_to_index(L):
+    """Converts a list to an integer value"""
+    if min(L) < 0:
+        raise ValueError("List must contain positive values only")
+    return sum([2 ** (elem - 1) for elem in L])
+
+
+def index_to_list(i):
+    """Converts an integer value to a list"""
+    if i < 0:
+        raise ValueError("Index value must be positive")
+    bits = []
+    while i > 0:
+        bits.append(i % 2)
+        i //= 2
+    return [i + 1 for i, j in enumerate(bits) if j == 1]
